91f84f0d7d7d55febdd1a0364463b06eab2019a8
ReyZL_ <scaryzack123@gmail.com>
2024-11-15 20:52:36 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function clicks() {
+    $('#div1').hide();
+    $('#div2').show();
+    $('#div2').addClass("fadeinv2");
+        
+    var typed = new Typed('.katakata', {
+        strings: ["Cuma mau bilang", "Selamat Malam Sayang ku", "Aku Sayang Banget Sama Kamu💑", "Semoga kamu ngga bosen bosen sama aku ya Say", "I Love You Anggi Putri Wulandari❤️"],
+        typeSpeed: 70
+    });
+}
